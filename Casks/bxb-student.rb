06755e3680d7ce758a25cdd8f158d78d85ecaea8
IGpig <igpig@IGpigdeMacBook-Pro-16281.local>
2026-06-11 19:11:36 +0800

    chore: update bxb-student to v1.3.0 (v1.3.1-beta)

--- Casks/bxb-student.rb
+++ Casks/bxb-student.rb
@@ -1,6 +1,6 @@
 cask "bxb-student" do
   version "1.3.0"
-  sha256 "61db152832dfdb92a2d9b0bfe23edc8c3e2fade8a61cc1d1513eef2859d007de"
+  sha256 "eb9a312bad321aa013f758446e494cf02de4a14ea8a34a0b0bb268aedfb76a52"
 
   url "https://github.com/GRAY-XY/BXB_tools/releases/download/v1.3.1-beta/BXB_Student_macOS_v1.3.0.dmg"
   name "BXB Student"
@@ -22,4 +22,3 @@ cask "bxb-student" do
     "~/Library/Logs/BXB Student",
   ]
 end
-
